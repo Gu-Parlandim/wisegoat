b68a10549a0e125aae0a363d04fddd723afa414a
Gu-Parlandim <gustavo.miguel.dias.parlanidm@gmail.com>
2022-01-05 23:29:31 -0300

    pequena mudança

--- js/main.js
+++ js/main.js
@@ -1,6 +1,28 @@
 const dados = () => {
     fetch("./js/frases.json")
     .then(res => res.json()).then(data =>{
-        console.log(data)
+        let frase = gerarFrase(data)
+        paginaConselho(frase)
         })
+} 
+
+
+gerarFrase = (dados) => {
+    let num = randomNumero()
+    return dados[num].frase
+}
+
+randomNumero = () => {
+    numeroAletorio = parseInt((Math.random() * 366))
+    return numeroAletorio
+}
+
+paginaConselho = (frase) => {
+    let pagina = document.querySelector("main")
+    pagina.innerHTML = 
+    `<div>
+        <h2>${frase}</h2>
+        <p>${"autor"}</p>
+        <button onclick="dados()"> Teste Apenas</button>
+    </div>`
 }
